6850da1c5411008a505fa4b6fcbbce8db0da8787
Kristaps Grinbergs <kristaps@qminderapp.com>
2016-01-31 15:37:31 +0200

    Added Crashlytics

--- TartuWeather/AppDelegate.swift
+++ TartuWeather/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
+  
+    Fabric.with([Crashlytics.self])
+    
     UINavigationBar.appearance().barStyle = .Black
+    
     return true
   }
 
