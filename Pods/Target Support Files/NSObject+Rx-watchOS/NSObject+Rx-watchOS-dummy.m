f9e660c6615d2f5b7bae725d54fc07033e4d1a45
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-01-14 15:39:59 +0200

    Remove Pods

--- Pods/Target Support Files/NSObject+Rx-watchOS/NSObject+Rx-watchOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NSObject_Rx_watchOS : NSObject
-@end
-@implementation PodsDummy_NSObject_Rx_watchOS
-@end
--- Pods/Target Support Files/Pods-meteoTartuUITests/Pods-meteoTartuUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_meteoTartuUITests : NSObject
-@end
-@implementation PodsDummy_Pods_meteoTartuUITests
-@end
